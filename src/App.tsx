3d9b48592f0d3bdc1b5ae3d1fd1a15036f9b1250
Chris <chris.fraanc@gmail.com>
2023-05-25 01:41:35 +0200

    [ADD] setting up the SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 // import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import SideBar from "@components/sideBar/SideBar";
 
 function App() {
-
   return (
     <>
-    <div className="project">
-      <h2 className="projectTitle">Projekopia</h2>
-      <p className="projectDescription">Projet trop cool qui va beaucoup servir</p>
-    </div>
+      <SideBar />
+      <div className="project">
+        <h2 className="projectTitle">Projekopia</h2>
+        <p className="projectDescription">
+          Projet trop cool qui va beaucoup servir
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/sideBar/SideBar.tsx
@@ -0,0 +1,9 @@
+const SideBar = () => {
+  return (
+    <div className="sideBarContainer">
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default SideBar;
