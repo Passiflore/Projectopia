d35f6888b25d21aed53d88db869463c7784743e3
Passiflore <elise.vue@hetic.net>
2023-05-25 01:05:36 +0200

    [ADD] Card container component
    Adding card container component
    Adding card container css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import CardContainer from './components/CardContainer/CardContainer'
 
 function App() {
 
@@ -8,6 +9,11 @@ function App() {
     <div className="project">
       <h2 className="projectTitle">Projekopia</h2>
       <p className="projectDescription">Projet trop cool qui va beaucoup servir</p>
+      <div className='projectStatuts'>
+        <CardContainer></CardContainer>
+        <CardContainer></CardContainer>
+        <CardContainer></CardContainer>
+      </div>
     </div>
     </>
   )
